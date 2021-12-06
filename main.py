d95bbf29e6202d682c9282d4529fffddddff4d50
zyij99 <91389500+zyij99@users.noreply.github.com>
2021-12-05 20:16:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,41 @@ import random
 import requests
 import uuid
 import shutil
+import praw         
 
 token = os.getenv('TOKEN')
 bot = commands.Bot(command_prefix='!', case_insensitive = False)
 bot.remove_command('help') #removes the default help command (replacing with my own)
 
+#https://www.youtube.com/watch?v=Q5u6MDQAG7I Followed tutorial
+reddit = praw.Reddit(client_id = 'khnzEfUFV1t6_6SS9O6a6w',
+                    client_secret = '-HV-qPI1IhY8UKQnsNaZ9LeFuV7NPw',
+                    username = os.getenv('usrname'),
+                    password = os.getenv('passwrd'),
+                    user_agent = 'usr_agent')
+subreddit = reddit.subreddit('memes')
+
+@bot.command()
+async def r_memes(ctx):
+    subreddit = reddit.subreddit('memes')
+    allsub = []
+
+    hot = subreddit.hot(limit = 100)
+    
+    for submission in hot:
+        allsub.append(submission)
+    random_sub = random.choice(allsub)
+    name = random_sub.title
+    url = random_sub.url
+    em = discord.Embed(title = name)
+
+    em.set_image(url = url)
+    await ctx.send(embed = em)
+
+
 @bot.group(invoke_without_command=True)
 #https://www.youtube.com/watch?v=ivXw9VO89jw & https://python.plainenglish.io/send-an-embed-with-a-discord-bot-in-python-61d34c711046
-async def help(msg):
+async def help(ctx):
     em = discord.Embed(title = 'Commands', description = 'Descriptions of commands and how to use them')
     em.add_field(name = 'image', value = 'Posts a saved image from the uncategorized directory corresponding to input num>0. \nFormat: !image <number>', inline=False)
     em.add_field(name = 'memes', value = 'Posts a saved image from the memes directory corresponding to input num>0. \nFormat: !memes <number>', inline=False)
@@ -21,7 +48,9 @@ async def help(msg):
     em.add_field(name = 's_img', value = 'Saves an image into the uncategorized directory. \nFormat: !s_img <UPLOADED IMG/DISCORD IMAGE LINK>', inline=False)
     em.add_field(name = 's_memes', value = 'Saves an image into the memes and uncategorized directory. \nFormat: !s_memes <UPLOADED IMG/DISCORD IMAGE LINK>', inline=False)
     em.add_field(name = 's_pet', value = 'Saves an image into the pet and uncategorized directory. \nFormat: !s_pet <UPLOADED IMG/DISCORD IMAGE LINK>', inline=False)
-    await msg.send(embed = em)
+    em.add_field(name = 'r_memes', value = 'Calls a random meme from the subreddit on Reddit.com that\'s in the hot 100 of posts. \nFormat: !r_memes', inline=False)
+
+    await ctx.send(embed = em)
 
 d = {}   #stores everything
 pet_dict = {} #stores pets
@@ -76,86 +105,86 @@ for line in f:
 f.close()
 
 @bot.command()
-async def randimg(msg):
+async def randimg(ctx):
     if len(d) == 0:
-        await msg.send('There are no images saved yet')
+        await ctx.send('There are no images saved yet')
     else:
         n = random.randint(1, len(d))
-        await msg.send(file = discord.File(d[n]))
-        await msg.send('This image is saved as image ' + str(n) + ' in the uncategorized directory')
+        await ctx.send(file = discord.File(d[n]))
+        await ctx.send('This image is saved as image ' + str(n) + ' in the uncategorized directory')
         f = open('memeURLs.txt', 'r')
         for line in f:
             if line == d[n]:
                 inv_meme = {value:key for key, value in meme_dict.items()} 
                 memeNum = str(inv_meme[line])
-                await msg.send('This image is saved as image ' + str(memeNum) + ' in the memes directory')
+                await ctx.send('This image is saved as image ' + str(memeNum) + ' in the memes directory')
         f.close()
         f = open('petsURLs.txt', 'r')
         for line in f:
             if line == d[n]:
                 inv_pets = {value:key for key, value in pet_dict.items()} 
                 petNum = str(inv_pets[line])
-                await msg.send('This image is saved as image ' + str(petNum) + ' in the pets directory')
+                await ctx.send('This image is saved as image ' + str(petNum) + ' in the pets directory')
         f.close()
         
 @bot.command()
-async def image(msg, num):
+async def image(ctx, num):
     if len(d) == 0:
-        await msg.send('There are no images saved yet. Use the !s_img, !s_pet, or !s_memes command to save images to the directory.')
+        await ctx.send('There are no images saved yet. Use the !s_img, !s_pet, or !s_memes command to save images to the directory.')
     elif num == '0':
         try:
-            msg.send(file = discord.File(d[num]))
+            ctx.send(file = discord.File(d[num]))
         except KeyError:
-            await msg.send('The image number currently ranges from 1 to ' + str(c))
+            await ctx.send('The image number currently ranges from 1 to ' + str(c))
     else:
         if int(num) > c:
-            await msg.send('The image can not be called. The range is currently 1 to ' + str(c))
+            await ctx.send('The image can not be called. The range is currently 1 to ' + str(c))
         else:
             num = int(num)
-            await msg.send(file = discord.File(d[num]))
+            await ctx.send(file = discord.File(d[num]))
 
 @bot.command()
-async def pet(msg, num):
+async def pet(ctx, num):
     if len(pet_dict) == 0:
-        await msg.send('There are no images saved yet. Use the !s_pet command to save images to the directory.')
+        await ctx.send('There are no images saved yet. Use the !s_pet command to save images to the directory.')
     elif num == '0':
         try:
-            msg.send(file = discord.File(pet_dict[num]))
+            ctx.send(file = discord.File(pet_dict[num]))
         except KeyError:
-            await msg.send('The image number currently ranges from 1 to ' + str(p))
+            await ctx.send('The image number currently ranges from 1 to ' + str(p))
     else:
         if int(num) > p:
-            await msg.send('The image can not be called. The range is currently 1 to ' + str(p))
+            await ctx.send('The image can not be called. The range is currently 1 to ' + str(p))
         else:
             num = int(num)
-            await msg.send(file = discord.File(pet_dict[num]))
+            await ctx.send(file = discord.File(pet_dict[num]))
 
 @bot.command()
-async def memes(msg, num):
+async def memes(ctx, num):
     if len(meme_dict) == 0:
-        await msg.send('There are no images saved yet. Use the !s_memes command to save images to the directory.')
+        await ctx.send('There are no images saved yet. Use the !s_memes command to save images to the directory.')
     elif num == '0':
         try:
-            msg.send(file = discord.File(meme_dict[num]))
+            ctx.send(file = discord.File(meme_dict[num]))
         except KeyError:
-            await msg.send('The image number currently ranges from 1 to ' + str(me))
+            await ctx.send('The image number currently ranges from 1 to ' + str(me))
     else:
         if int(num) > me:
-            await msg.send('The image can not be called. The range is currently 1 to ' + str(me))
+            await ctx.send('The image can not be called. The range is currently 1 to ' + str(me))
         else:
             num = int(num)
-            await msg.send(file = discord.File(meme_dict[num]))
+            await ctx.send(file = discord.File(meme_dict[num]))
 
 #Source: https://www.youtube.com/watch?v=pgmUBOV3IIs
 @bot.command()
-async def s_img(msg):
+async def s_img(ctx):
     #!update in the comment when uploading an image to save the image as a .jpg
     #exception block to check for error
     try:
-        url = msg.message.attachments[0].url            # check for an image, call exception if none found
+        url = ctx.message.attachments[0].url            # check for an image, call exception if none found
     except IndexError:
         print("Error: No attachments")
-        await msg.send("No attachments detected!")
+        await ctx.send("No attachments detected!")
     else:
         if url[0:26] == "https://cdn.discordapp.com":   # Checks to see if url is from discord
             r = requests.get(url, stream=True)
@@ -166,21 +195,21 @@ async def s_img(msg):
                 global c
                 c += 1
                 d[c] = imgName
-                await msg.send('Saved Image' , file = discord.File(d[c]))
+                await ctx.send('Saved Image' , file = discord.File(d[c]))
                 inv_d = {value:key for key, value in d.items()} #https://stackoverflow.com/questions/8023306/get-key-by-value-in-dictionary?page=2&tab=votes#tab-top
                 imgNum = str(inv_d[imgName])
-                await msg.send('This is image ' + imgNum)
+                await ctx.send('This is image ' + imgNum)
                 with open('imgURLs.txt', 'a') as a_file:    #https://www.kite.com/python/answers/how-to-append-a-newline-to-a-file-in-python
                     a_file.write('\n' + str(imgName))   # writes the image name into a txt document, which can be used to recall images loaded into the dictionary
                 return c
 
 @bot.command()
-async def s_pet(msg):
+async def s_pet(ctx):
     try:
-        url = msg.message.attachments[0].url           
+        url = ctx.message.attachments[0].url           
     except IndexError:
         print("Error: No attachments")
-        await msg.send("No attachments detected!")
+        await ctx.send("No attachments detected!")
     else:
         if url[0:26] == "https://cdn.discordapp.com":   
             r = requests.get(url, stream=True)
@@ -194,12 +223,12 @@ async def s_pet(msg):
                 p += 1
                 d[c] = imgName
                 pet_dict[p] = imgName
-                await msg.send('Saved Image' , file = discord.File(d[c]))
+                await ctx.send('Saved Image' , file = discord.File(d[c]))
                 inv_d = {value:key for key, value in d.items()} 
                 imgNum = str(inv_d[imgName])
                 inv_pet = {value:key for key, value in pet_dict.items()} 
                 petNum = str(inv_pet[imgName])
-                await msg.send('This is image ' + imgNum + ' of the unsorted directory and is image ' + petNum + ' of the pets directory.')
+                await ctx.send('This is image ' + imgNum + ' of the unsorted directory and is image ' + petNum + ' of the pets directory.')
                 with open('petsURLs.txt', 'a') as a_file:  
                     a_file.write('\n' + str(imgName))
                 with open('imgURLs.txt', 'a') as a_file:  
@@ -207,12 +236,12 @@ async def s_pet(msg):
                 return c and p
 
 @bot.command()
-async def s_memes(msg):
+async def s_memes(ctx):
     try:
-        url = msg.message.attachments[0].url           
+        url = ctx.message.attachments[0].url           
     except IndexError:
         print("Error: No attachments")
-        await msg.send("No attachments detected!")
+        await ctx.send("No attachments detected!")
     else:
         if url[0:26] == "https://cdn.discordapp.com":   
             r = requests.get(url, stream=True)
@@ -226,12 +255,12 @@ async def s_memes(msg):
                 me += 1
                 d[c] = imgName
                 meme_dict[me] = imgName
-                await msg.send('Saved Image' , file = discord.File(d[c]))
+                await ctx.send('Saved Image' , file = discord.File(d[c]))
                 inv_d = {value:key for key, value in d.items()} 
                 imgNum = str(inv_d[imgName])
                 inv_meme = {value:key for key, value in meme_dict.items()} 
                 memeNum = str(inv_meme[imgName])
-                await msg.send('This is image ' + imgNum + ' of the unsorted directory and is image ' + memeNum + ' of the memes directory.')
+                await ctx.send('This is image ' + imgNum + ' of the unsorted directory and is image ' + memeNum + ' of the memes directory.')
                 with open('memeURLs.txt', 'a') as a_file:  
                     a_file.write('\n' + str(imgName))
                 with open('imgURLs.txt', 'a') as a_file:  
